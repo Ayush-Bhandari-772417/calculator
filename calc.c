2a88363f10be43ad0ca6ec4180b7165bca7ca4ce
Ayush Bhandari <er.st.ayushbhandari@gmail.com>
2022-02-25 14:07:27 +0545

    third commit from another branch

--- calc.c
+++ calc.c
@@ -3,3 +3,5 @@ Program Name: calculator.c
 Program Description: this is a calculator project.
 Program Author: <Ayush Bhandari>
 ********************************************************************************************************/
+
+#include <stdio.h> //from experiment branch
